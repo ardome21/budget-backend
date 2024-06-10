b391940bc31e1ba37e4afca0f073336001758721
ardome21 <ardome21@gmail.com>
2024-06-10 11:46:40 -0400

    Get rid of warning, and getter,setter,ctor

--- src/main/java/com/example/budgetbackend/model/Transaction.java
+++ src/main/java/com/example/budgetbackend/model/Transaction.java
@@ -1,10 +1,14 @@
 package com.example.budgetbackend.model;
 
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.util.Date;
 
+@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class Transaction {
--- src/main/java/com/example/budgetbackend/model/TransactionItem.java
+++ src/main/java/com/example/budgetbackend/model/TransactionItem.java
@@ -2,14 +2,15 @@ package com.example.budgetbackend.model;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class TransactionItem {
     private String description;
     private String category;
     private double amount;
-
 }
